a347448568b9de77c1155e3e67bdbf48d9f0a3cd
Hollie Baker <hollieabaker@gmail.com>
2021-02-15 17:24:09 +0000

    Add cell dimensions to outputx

--- source/io/CELLWRT.c
+++ source/io/CELLWRT.c
@@ -9,23 +9,42 @@ Write out the cell C as a tarski formula
 
 void QepcadCls::CELLWRT(Word c)
 {
-       Word S,S1,k,t,i,D,M;
-       /* hide t; */
+        Word S,S1,k,t,i,D,I;
+        /* hide t; */
 
 Step1: /* Heading. */
-       k = LELTI(c,LEVEL);
-       SWRITE("---------- Information about the cell "); LWRITE(LELTI(c,INDX));
-       SWRITE(" ----------\n\n");
+        k = LELTI(c,LEVEL);
+        SWRITE("---------- Information about the cell "); LWRITE(LELTI(c,INDX));
+        SWRITE(" ----------\n\n");
 
-Step2: /* Signs of Projection Factors. */
-       S = LELTI(c,SIGNPF);
-       CELLIPLLDWR(GVVL, GVPF, S, k); SWRITE("\n"); goto Return;
+Step2: /* Dimension */
+        SWRITE("Dimension ");
 
-Step3: /* Finish. */
-       SWRITE("\n----------------------------------------------------\n");
-       goto Return;
+        int d = 0, o = 0;
+        I = LELTI(c,INDX);
+        i = NIL;
+        while (I != NIL) {
+            SWRITE(i == NIL ? "(" : ",");
+            ADV(I, &i, &I);
+            o = ODD(i);
+
+            d += o;
+
+            IWRITE(o);
+        }
+        SWRITE(") ");
+        IWRITE(d);
+        SWRITE("\n\n");
+
+Step3: /* Signs of Projection Factors. */
+        S = LELTI(c,SIGNPF);
+        CELLIPLLDWR(GVVL, GVPF, S, k); SWRITE("\n"); goto Return;
+
+Step4: /* Finish. */
+        SWRITE("\n----------------------------------------------------\n");
+        goto Return;
 
 Return: /* Prepare for return. */
-       return;
+        return;
 }
 

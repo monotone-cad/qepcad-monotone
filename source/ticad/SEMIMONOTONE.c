ee2aa4d24bf3d6ae0400d453ab7e1a1efa1c6fa6
Hollie Baker <hollieabaker@gmail.com>
2021-02-19 18:33:56 +0000

    adding partials of projection factors

--- source/ticad/SEMIMONOTONE.c
+++ source/ticad/SEMIMONOTONE.c
@@ -15,6 +15,45 @@ Adds extra polynomials to projection factor set to ensure semi-monotone cells wi
 
 Word QepcadCls::SEMIMONOTONE(Word A, Word r)
 {
-    printf("in semimonotone!!");
-    return A;
+    Word P, L, L2, A1, p, d, W;
+
+    if (r != 3) return A;
+    // TODO handle r > 3
+    printf("in semimonotone!!");fflush(0);
+
+Step1: /* add derivatives of projection factors */
+    P = A;
+    L2 = NIL;
+    int k = 0;
+    while (A != NIL)  {
+        ADV(A, &L, &A);
+        k++;
+        // skip level r and level 1
+        if (k == 1 || k == r) continue;
+
+        while (L != NIL) {
+            ADV(L, &A1, &L);
+
+            p = LELTI(A1, PO_POLY);
+
+            d = IPDER(k, p, 1);
+
+            if (IPCONST(k, d)) continue;
+
+            W = MPOLY(d, NIL, NIL, PO_OTHER, PO_KEEP);
+
+            IPDWRITE(k, d, GVVL); SWRITE("\n");
+            L2 = COMP(W, L2);
+        }
+
+        printf("  %d\n", LENGTH(L2));
+    }
+
+    // using append from projection to handle the labels
+    L2 = IPLFAC(r-1, L2);
+    P = APPEND(P, r-1, L2);
+
+
+Return: /* return */
+    return P;
 }

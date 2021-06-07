68c2a704815204a3bc0c8a40d4e33db7af03cd4c
Hollie Baker <hollieabaker@gmail.com>
2021-06-07 14:50:28 +0000

    update cell splitting code to handle algebraic sample / splittitng points

--- source/ticad/RECOMPUTE.c
+++ source/ticad/RECOMPUTE.c
@@ -32,11 +32,17 @@ Word ADVN(Word L, Word n);
 // -1 if < 0, 1 if > 0, 0 if there is a root within the cell
 Word INTERVALSIGN(Word Cl, Word C, Word Cr, Word x);
 
+// -1 if < 0, 1 if > 0. same as intervalsign, but for section cells
+Word CELLSIGN(Word C, Word x);
+
 // get the k-th coordinate of sample point of C
 Word SAMPLEK(Word C, Word k);
 
-// find the solution of a linear polynomial p
-Word LINEARROOT(Word p);
+// solution of a polynomial as algebraic number
+// if 0 then 0,
+// if rational then algebraic representation of rational number
+// otherwise (b, M, I) where M = P, I is the isolating interval, and b = f(x) = x
+Word ROOTS(Word p);
 
 // Append sign of projection factor to C at (C, LEVEL)
 void APPENDSIGNPF(Word C, Word S);
@@ -55,8 +61,11 @@ Word SPLITCELL(Word C, Word p, Word Sl, Word Sr);
 // insert new cells into list L
 Word INSERTCELLS(Word L, Word NewCells);
 
-// algebraic number (b-a)/2
-Word MIDPOINT(Word M, Word l, Word r);
+// "mid" point of two pseudo sample points.
+// if either l or r is not rational, then we take the inside end of isolating interval,
+// otherwise, both are rational, and we take the fraction (a-b)/2
+// returns a pseudo sample point, which is rational
+Word MIDPOINT(Word l, Word Ml, Word Il, Word r, Word Mr, Word Ir);
 
 // set sample coordinate, level k, to m
 void SETSAMPLEK(Word C, Word k, Word m);
@@ -99,7 +108,7 @@ Word QepcadCls::RECOMPUTE(Word C, Word Q, Word F, Word f, Word P, Word A)
 
 Word SPLIT(Word L, Word Ps)
 {
-    Word P, p, x, s, C, Next, Prev, Lp, Lp1, Lc, Lc1, Ln, Ln1, NewCells, k;
+    Word P, p, xs, xs1, x, s, C, Next, Prev, Lp, Lp1, Lc, Lc1, Ln, Ln1, NewCells, k;
 
     // initialising variables. we assume all cells in the list have the same level, we also assume that the new proj
     // factors with missing signs are at the end of the list, and that all cells have the same number of proj factors so
@@ -115,24 +124,35 @@ Word SPLIT(Word L, Word Ps)
     Lc = LCOPY(L), Lp = Lc, Ln = INV(Lc);
     Lp = COMP(FIRST(Lp), Lp); // = [a1, a1, ..., an], Note we won't need an
     Ln = RED(INV(COMP(FIRST(Ln), Ln))); // = [a2, ..., an, an]
-    printf("%d %d %d %d\n", LENGTH(L), LENGTH(Lp), LENGTH(Lc), LENGTH(Ln));
 
-Step1: /* Iterate through the new polynomials */
+    /* find roots of the new polynomials represented as algebraic numbers */
+    xs = NIL;
+
     while (Ps != NIL) {
         ADV(Ps, &P, &Ps);
         p = LELTI(P, PO_POLY);
-        x = LINEARROOT(p);
+        xs1 = ROOTS(p);
 
-        if (x == NIL) {
+        if (xs1 == NIL) {
             SWRITE("ERROR: unable to find a root of new polynomial.\n");
 
             return L;
         }
 
-        // convert to algebraic extension representation, to be consistent with sample points
-        x = AFFRN(x);
+        xs = CONC(xs, xs1);
+    }
+
+    printf("number of roots: %d\n", LENGTH(xs));
+    /* iterate through the new roots, add new signatures and split if needed */
+    while (xs != NIL) {
+        ADV(xs, &x, &xs);
+        SWRITE("### splitting point: ");
+        Word Ix = THIRD(x);
+        SWRITE("I=("); RNWRITE(FIRST(Ix)); SWRITE(", "); RNWRITE(SECOND(Ix)); SWRITE(") "); LWRITE(SECOND(x));
+        SWRITE(" "); RNWRITE(FIRST(FIRST(x))); SWRITE("\n");
 
         NewCells = NIL, Lp1 = Lp, Lc1 = Lc, Ln1 = Ln, Prev = NIL, C = NIL, Next = NIL;
+        bool section = true; // currently looking at a section cell? (initially we are looking at -\infty)
         // LENGTH(Ln) == LENGTH(L) - Ln is the only list having the correct length
         // since Lc has concatenated last element (from Ln) and Lp has concatenated first and last elements
         // this is the price of pass by reference I guess :(
@@ -141,14 +161,20 @@ Step1: /* Iterate through the new polynomials */
             ADV(Lp, &Prev, &Lp);
             ADV(Lc, &C, &Lc);
             ADV(Ln, &Next, &Ln);
+            section = !section;
 
-            Prev != NIL ?LWRITE(LELTI(Prev, INDX)) : SWRITE("NIL"); SWRITE(" ");
-            C != NIL ? LWRITE(LELTI(C, INDX)) : SWRITE("NIL"); SWRITE(" ");
-            Next != NIL ? LWRITE(LELTI(Next, INDX)) : SWRITE("NIL"); SWRITE("\n");
+            // section cell, append new sign
+            if (section) {
+                APPENDSIGNPF(C, CELLSIGN(C, x));
 
+                continue;
+            }
+
+            // sector cell. may contain splitting point, otherwise just add the sign
             s = INTERVALSIGN(Prev, C, Next, x);
             if (s == 0) {
                 // we have a change of sign within C
+                printf("splitting cell "); LWRITE(LELTI(C, INDX)); printf("\n");
                 NewCells = SPLITCELL(
                     C,
                     x,
@@ -158,8 +184,9 @@ Step1: /* Iterate through the new polynomials */
 
                 // add new cells to list, replacing any cells that were split by a new polynomial
                 // TODO hopefully editing L won't screw things up
+                // maybe we can append newCells to the end and add all at once??
                 L = INSERTCELLS(L, NewCells);
-            } else { // it's sign invarient already, simply add the new sign
+            } else {
                 APPENDSIGNPF(C, s);
             }
         }
@@ -182,12 +209,6 @@ Word ADVN(Word L, Word n)
 
 Word LINEARROOT(Word p)
 {
-    Word n = PDEG(p);
-
-    // not linear
-    if (n > 1) return NIL; // TODO what to do if it's not linear
-
-    // directly solve P := den X + num = 0
     Word den = PLDCF(p);
     Word num = PCOEFF(p,0);
 
@@ -200,7 +221,49 @@ Word LINEARROOT(Word p)
     if (den == 0) return NIL;
 
     // directly solve x = -num/den
-    return RNRED(INEG(num),den);
+    return AFFRN(
+        RNRED(INEG(num),den)
+    );
+}
+
+Word ROOTS(Word p)
+{
+    Word n = PDEG(p);
+
+    // don't think this can happen, but just in case
+    if (n == 0) {
+        return NIL;
+    }
+
+    // P is linear: directly solve P := den X + num = 0
+    // return in pseudo-sample point representation
+    if (n == 1) {
+        return LIST1(
+            LIST3(
+                LINEARROOT(p),
+                PMON(1,1),
+                LIST2(0,0)
+            )
+        );
+    }
+
+    // not linear: find isolating intervals for the roots and return in rational number form
+    // we will use P as the minimal polynomial. beta = P(alpha)
+    Word B = LIST2(LIST2(1,1), PMON(1,1)); // univariate polynomial: (1/1)(1*x^1)
+    Word Is = IPRIM(p);
+    Word M = NIL, I = NIL, J = NIL;
+    while (Is != NIL) {
+        ADV(Is, &I, &Is);
+        // refine the isolating interval I // TODO what should p be?
+        J = LBRIBRI(ANR(1, p, BRILBRI(I))); // TODO is there a way of avoiding all the logarithmic conversion and back?
+
+        M = COMP(
+            LIST3(B, p, J),
+            M
+        );
+    }
+
+    return INV(M);
 }
 
 Word SAMPLEK(Word C, Word k)
@@ -217,39 +280,120 @@ Word SAMPLEK(Word C, Word k)
     return LIST3(LELTI(P, k), M, I);
 }
 
-Word INTERVALSIGN(Word Cl, Word C, Word Cr, Word x)
+inline Word RATIONALPART(Word x)
 {
-    Word k, idx, Sl, Sr, l, r, Ml, Il, Mr, Ir;
+    return x == 0 ? 0 : FIRST(x);
+}
 
-Step1: /* Initialise */
-    k = LELTI(C, LEVEL);
-    idx = LELTI(LELTI(C, INDX), k);
+Word CONVERTPOLYNOMIAL(Word P)
+{
+    Word e, a;
+    Word P1 = NIL;
+    while (P != NIL) {
+        ADV2(P, &e, &a, &P);
+        P1 = COMP2(AFFINT(a), e, P1);
+    };
 
-    // special case for even (section) cell
-    // we set cells on either side to the value of C
-    if (EVEN(idx)) {
-        Sl = SAMPLEK(C, k);
-        Sr = Sl;
-    } else {
-        Sl = SAMPLEK(Cl, k);
-        Sr = SAMPLEK(Cr, k);
-        // note that we padded the cell list so that leftmost and rightmost cells will have Cl == C and Cr == C resp.
+    return INV(P1);
+}
+
+// algebraic number (not rational) comparison
+// since r is splitting point, we may assume that beta == alpha.
+Word ANCOMPARE(Word Mx, Word Ix, Word y, Word My, Word Iy)
+{
+    // TODO something with the isolating interval for efficiency, before messing about with algebraic numbers
+
+    // compute the common extension Gamma, and comvert y into that
+    Word G, J, t, u, a, b, junk, L, a1, a2;
+    // convert My into Q(alpha)[y] representation.
+    My = CONVERTPOLYNOMIAL(My);
+    // printf("Mx = "); LWRITE(Mx); printf(" ");
+    // printf("My = "); LWRITE(My); printf("\n");
+
+    SIMPLEQE(Mx, Ix, My, Iy, &G, &t, &u, &J, &a, &b, &junk, &junk);
+    MODCRDB(LIST1(y), G, a, b, &L);
+    FIRST2(L, &a1, &a2);
+    // printf("t = %d, a = ", t); (a == 0 ? IWRITE(0) : LWRITE(a)); printf(" ");
+    // printf("u = %d; b = ", u); (b == 0 ? IWRITE(0) : LWRITE(b)); printf("\n");
+    // printf("GAMMA = "); LWRITE(G); printf(" ");
+    // printf("J = "); LWRITE(J); printf("\n");
+    // printf("AFCOMP of ");
+    // (a1 == 0 ? IWRITE(0) : LWRITE(a1));
+    // printf(" ");
+    // (a2 == 0 ? IWRITE(0) : LWRITE(a2));
+    // printf("\n");
+
+    // -1 because x and y are swapped // TODO maybe swap it back so it is less confusing?
+    return -AFCOMP(G, J, a1, a2);
+}
+
+// helper function for comparing two pseudo-sample points. R is our "splitting point" and S is the sample point of the
+// cell we are examining
+Word COMPARE(Word R, Word S)
+{
+    const Word Px = PMON(1,1); // P(x) = x - this is the convention used for an algebraic representation of a rational number
+    Word x, Mx, Ix, y, My, Iy;
+
+    // get coordinate, minimal polynomial, and isolating interval
+    FIRST3(R, &x, &Mx, &Ix);
+    FIRST3(S, &y, &My, &Iy);
+
+    // we determine the type of point (algebraic extension or rational) to figure out which comparison we can do
+    bool x_rat = PDEG(Mx) == 1;
+    bool y_rat = PDEG(My) == 1;
+
+    // both are rational: easy! just RNCOMP the rational bit
+    if (x_rat && y_rat) {
+        return RNCOMP(
+            RATIONALPART(y),
+            RATIONALPART(x)
+        );
     }
 
-    /* endpoint comparison */
-    // get value, M, I for each point
-    FIRST3(Sl, &l, &Ml, &Il);
-    FIRST3(Sr, &r, &Mr, &Ir);
+    // both algebraic (non rational)
+    if (!x_rat && !y_rat) {
+        return ANCOMPARE(
+            Mx, Ix,
+            y, My, Iy
+        );
+    }
 
-    // comparisons
-    Word xl = AFCOMP(Ml, Il, x, l); // x is: left < equal < right of l
-    Word xr = AFCOMP(Mr, Ir, x, r); // x is: less < equal < right of r
+    // one is rational, we can use the minimal polynomial and rational point to figure out the sign
+    Word ratPart = RATIONALPART(x_rat ? x : y);
+    Word minPol = x_rat ? My : Mx;
 
-    // decide whether our x is inside, left of, or right of our interval
-    if (xl > 0 && xr < 0) return 0;
-    if (xr <= 0) return 1;
-    if (xl >= 0) return -1;
-    return NIL; // if this happened something went badly wrong
+    // the sign of M at x
+    Word s = IUPBES(minPol, ratPart);
+    // may need to flip the sign if M and x are the other way around
+    return s * (x_rat ? -1 : 1);
+}
+
+Word CELLSIGN(Word C, Word R)
+{
+    Word k = LELTI(C, LEVEL);
+    Word S = SAMPLEK(C, k);
+
+    return COMPARE(R, S);
+}
+
+Word INTERVALSIGN(Word Cl, Word C, Word Cr, Word x)
+{
+    Word k = LELTI(C, LEVEL);
+    Word Sl = SAMPLEK(Cl, k);
+    Word Sr = SAMPLEK(Cr, k);
+
+    // TODO remove, some day.
+    int comp_l = COMPARE(x, Sl);
+    int comp_r = COMPARE(x, Sr);
+
+    // TODO is there a less iffy way of doing that?
+    if (COMPARE(x, Sl) > 0) { // right of left endpoint, + on cell
+        return 1;
+    } else if (COMPARE(x, Sr) < 0) { // left of right endpoint, - on cell
+        return -1;
+    } else { // middle
+        return 0;
+    }
 }
 
 void APPENDSIGNPF(Word C, Word sign)
@@ -335,13 +479,31 @@ Word CELLCOPY(Word C)
 
 void SETSAMPLEK(Word C, Word k, Word m)
 {
-    Word S = LELTI(LELTI(C, SAMPLE), 3);
-    SLELTI(S, k, m);
+    const Word Px = PMON(1,1);
+    Word S, Ms, Is, B, My, Iy, y;
+    S = LELTI(C, SAMPLE);
+    FIRST3(S, &Ms, &Is, &B); // qepcad sample point
+    FIRST3(m, &y, &My, &Iy); // pseudo-sample point (yes it's confusing, should probably change it to be consistent)
+
+    // set coordinate k
+    SLELTI(B, k, y);
+
+    // set the new algebraic extension
+
+    if (PDEG(My) == 1) return; // we're good, new point is rational
+
+    if (PDEG(Ms) == 1) { // all other points are rational, we can overwrite M and I.
+        SLELTI(S, 1, My);
+        SLELTI(S, 2, Iy);
+    } else { // we will have to find a new algebraic extension encompassing both M and M1.
+        // TODO
+        printf("TODO: comput a new algebraic extension\n");
+    }
 }
 
 Word SPLITCELL(Word C, Word x, Word Sl, Word Sr)
 {
-    Word Cl, Cr, k, M, l, r, m;
+    Word Cl, Cr, k, M, I, b, m, xx, Mx, Ix;
 
     k = LELTI(C, LEVEL);
     Cl = CELLCOPY(C);
@@ -352,18 +514,19 @@ Word SPLITCELL(Word C, Word x, Word Sl, Word Sr)
     INCINDEX(Cr, k, 2);
 
     // set sample points
+    FIRST3(x, &xx, &Mx, &Ix);
     // Sl : less than x
-    FIRST2(Sl, &l, &M);
-    m = MIDPOINT(M, l, x);
+    FIRST3(Sl, &b, &M, &I);
+    m = MIDPOINT(b, M, I, xx, Mx, Ix);
     SETSAMPLEK(Cl, k, m);
 
     // C : equal to x
     SETSAMPLEK(C, k, x);
 
     // Cr : greater than x
-    FIRST2(Sr, &r, &M);
-    m = MIDPOINT(M, x, r);
-    SETSAMPLEK(Cr, k, m);
+    FIRST3(Sr, &b, &M, &I);
+    m = MIDPOINT(xx, Mx, Ix, b, M, I);
+    SETSAMPLEK(Cl, k, m);
 
     // add signs of new projection factor
     APPENDSIGNPF(Cl, -1);
@@ -397,13 +560,28 @@ Word INSERTCELLS(Word LL, Word NewCells)
     return LL;
 }
 
-Word MIDPOINT(Word M, Word l, Word r)
+Word MIDPOINT(Word l, Word Ml, Word Il, Word r, Word Mr, Word Ir)
 {
-    Word two = AFFINT(2);
-    return AFSUM(l, AFQ(
-        M,
-        AFDIF(r, l),
-        two
-    ));
+    Word Px = PMON(1,1);
+    Word m;
+
+    // if endpoint is rational, use that, if not, use the endpoint of isolating interval
+    // convert to binary rational representation for later.
+    if (PDEG(Ml) > 1) {
+        l = SECOND(Il);
+    }
+
+    if (PDEG(Mr) > 1) {
+        r = FIRST(Ir);
+    }
+
+    // return midpoint of two rational endpoints (approximate, returning a simple fraction)
+    m = LBRNRN(SLBRNIM(RNLBRN(l), RNLBRN(r)));
+
+    return LIST3(
+        AFFRN(m),
+        Px,
+        LIST2(0,0)
+    );
 }
 

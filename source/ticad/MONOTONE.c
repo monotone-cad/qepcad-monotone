8c50c3266f59b532f4f6a47bca7256d219418d62
Hollie Baker <hollieabaker@gmail.com>
2023-07-25 15:43:28 +0100

    wip

--- source/ticad/MONOTONE.c
+++ source/ticad/MONOTONE.c
@@ -1,8 +1,8 @@
 /*======================================================================
-                    F -< MONOTONE(FF)
+                   F -< MONOTONE(FF)
 
 Adds extra polynomials to projection factor set to ensure monotone cells will be produced
-Does not recompute the cad
+A refinement of the kind { x_i < c }, { x_i = c }, { x_i > c } will be required.
 
 \Input
   \parm{A} is a list a_1, ..., a_r where a_i is a list of i-level projection factors
@@ -13,17 +13,17 @@ Does not recompute the cad
 ======================================================================*/
 #include "qepcad.h"
 
-// for retrieving one-dimensional components of two-dimensional cell indices
-// let I = (0,...,0,i_j=1,0,...,0,i_k=1,0,...,0)
-// if C is two-dimensional, then I is of this form.
-// Then j and k are set accordingly, true is returned.
-// Otherwise, false is returned, value of j and k are undefined.
+// suppose I = (i_1,...,i_n) is a cell index.
+// if i_j = i_k = 1 but all other components are 0, then C is 2-dimensional.
+// set j and k to indicate which components are equal to one and return true.
+// otherwise, C is not two-dimensional. return false. Values of j and k are not defined.
 bool TwoDimIndex(Word I, Word *j_, Word *k_)
 {
     Word J,d,n,l;
 
+    // d tracks cell dimension.
     n = 0, d = 0;
-    while (I != NIL) {
+    while (d <= 3 && I != NIL) {
         ADV(I, &l, &I);
         ++n;
 
@@ -31,23 +31,24 @@ bool TwoDimIndex(Word I, Word *j_, Word *k_)
             ++d;
             J = COMP(n, J);
         }
-
-        // only consider 2-dimensional cells
-        if (d > 2) {
-            return false;
-        }
     }
 
-    if (d < 2) {
-        return false;
+    // if cell is two-dimensional
+    if (d == 2) {
+        FIRST2(J, k_, j_);
+
+        return true;
     }
 
-    // assign j and k, and return
-    FIRST2(J, k_, j_);
-    return true;
+    // otherwise cell is not two-dimensional
+    return false;
 }
 
 // return the cell in list L with (partial) index I.
+// purpose is to find parent cells.
+// L : list of cells, level k
+// I : partial index, level j
+// return : C s.t. C has index I
 Word FindByIndex(Word L, Word I, Word j, Word k)
 {
     Word J,C,C1;
@@ -59,57 +60,94 @@ Word FindByIndex(Word L, Word I, Word j, Word k)
         // no partial match
         if (FIRST(I) != LELTI(J, k)) continue;
 
-        // found
+        // otherwise, partial match. if j == k then we're done
         if (j == k) return C;
 
-        // partial match, recursion
+        // if j < k, then we have a partial match but need to keep on searching recursively
         return FindByIndex(LELTI(C, CHILD), RED(I), j, k + 1);
     }
 
-    // not found by recursion
+    // no partial match.
     return NIL;
 }
 
 // return a list (f_j,...,f_k), where f_i is a level j polynomial which is 0 on C.
+// A : reversed list of projection factors
+// r : ambient dimension of CAD
+// C : cell to check
+// i1 <= i1 : min and max indices to search
+// return : (f_{i_1}, ..., f_{i2})
 Word ZeroPols(Word A, Word r, Word C, Word i1, Word i2)
 {
-    Word k, L, A1, P, S, S1, s;
+    Word k, L, A1, P, Q, S, S1, s;
     k = LELTI(C, LEVEL);
     S = REDI(LELTI(C, SIGNPF), k - i2);
     A = REDI(A, r-i2); // throw out polynomials of higher level
-    printf("%d %d %d\n", k, LENGTH(S), LENGTH(A));
     L = NIL;
 
     while (A != NIL && S != NIL) {
         // only look for requested polynomials
         if (i2 < i1) break;
-        printf("round %d ", i2);
+
         ADV(A, &A1, &A);
         ADV(S, &S1, &S);
 
-        // check signs, lvl k.
+        // check signs of level k polynomials
+        Q = 0;
         while (A1 != NIL && S1 != NIL) {
             ADV(A1, &P, &A1);
             ADV(S1, &s, &S1);
-            LWRITE(LELTI(P, PO_POLY)); printf(" [%d] ", s); SWRITE(" ");
 
             if (s == ZERO) {
-                L = COMP(LELTI(P, PO_POLY), L);
+                Q = LELTI(P, PO_POLY);
+
+                break;
             }
         }
-        SWRITE("\n");
-        // what if none were found?
+
+        // append polynomial.
+        // note 1: if none found, then 0 is appended.
+        // note 2: if more than one polynomial is zero on C, then only the first in the list is added
+        L = COMP(Q, L);
         --i2;
     }
-    printf("%d\n", LENGTH(L));
-    return L; // note that l is in ascending order.
+
+    return L; // note: L is in ascending level order.
+}
+
+
+
+// evaluate r-variate integer polynomial P at sample point S = (c_1,...,c_k)
+// return Q in Z[x_{k+1},...,k_r] = P(c_1,...,c_k,x_{k+1},...,k_r)
+// r : positive integer
+// P : integer polynomial in Z[x_1,...,x_r]
+// S : sample point (Q, I, (c_1,...,c_k)), in primitive form
+// return : substituted polynomial in Z[x_{k+1},...,x_r]
+Word Substitute(Word r, Word P, Word S)
+{
+    Word LL, P1, Q, J, c;
+
+    // determine if S is rational or algebraic
+    FIRST3(S, &Q, &J, &c);
+
+    // sample subset R^0, nothing to do
+    if (LENGTH(c) == 0) return P;
+    LWRITE(P); SWRITE("\n");
+
+    if (PDEG(Q) == 1) {
+        LWRITE(RCFAFC(c)); SWRITE("\n");
+        P1 = IPFRP(r - LENGTH(c), IPRNME(r, P, RCFAFC(c)));
+    }
+    LWRITE(P1); SWRITE("\n");
+
+    return P1;
 }
 
 void QepcadCls::MONOTONE(Word A, Word J, Word D, Word r)
 {
-    Word AA, Ds, TrueCells, junk, C, I, I1, Ij, Ik, C0, CT, CB, Gs, Fk, Fs, P, P1;
+    Word AA, Ds, TrueCells, junk, C, I, I1, Ij, Ik, C0, S0, CT, CB, Gs, FT, FB, Fs, P, P1;
 
-    // loop over true cells
+    // consider true cells
     LISTOFCWTV(D, &TrueCells, &junk);
     Ds = LELTI(D, CHILD); // cells of D, for searching cells
     AA = INV(A); // in reverse order, to match SIGNPF. for later.
@@ -118,7 +156,7 @@ void QepcadCls::MONOTONE(Word A, Word J, Word D, Word r)
         ADV(TrueCells, &C, &TrueCells);
         I = LELTI(C, INDX);
 
-        // consider 2-dimensional cells.
+        // only consider two-dimensional cells
         if (!TwoDimIndex(I, &Ij, &Ik)) continue;
 
         // TODO
@@ -126,23 +164,27 @@ void QepcadCls::MONOTONE(Word A, Word J, Word D, Word r)
         LWRITE(I);
         printf(" 2d indx: (%d, %d)\n", Ij, Ik);
 
-        // proj_j(C) is a 0-dimensional cell (c_1,...,c_{j-1})
+        // C0 := proj_{j-1}(C) is a 0-dimensional cell (c_1,...,c_{j-1})
         if (Ij > 1) {
             C0 = FindByIndex(Ds, I, Ij-1, 1);
         } else {
             C0 = D;
         }
 
-        // top and bottom of proj_k(C) are one-dimensional sections.
+        // sample point (c_1,...,c_{j-1}) of C0.
+        S0 = LELTI(C0, SAMPLE);
+
+        // top and bottom of proj_k(C) are one-dimensional sections by definition.
         I1 = LCOPY(I);
-        // top
+        // top: (i_1,...,i_k + 1)
         SLELTI(I1, Ik, LELTI(I, Ik) + 1);
         CT = FindByIndex(Ds, I1, Ik, 1);
-        // bottom
+        // bottom: (i_1,...,i_k - 1)
         SLELTI(I1, Ik, LELTI(I, Ik) - 1);
         CB = FindByIndex(Ds, I1, Ik, 1);
 
         // TODO
+        printf("sub-cad level "); IWRITE(LELTI(C, LEVEL)); SWRITE(", ");
         printf("sub-cad index "); LWRITE(LELTI(C0, INDX)); SWRITE("\n");
         printf("top C index "); LWRITE(LELTI(CT, INDX)); SWRITE("\n");
         printf("bottom C index "); LWRITE(LELTI(CB, INDX)); SWRITE("\n");
@@ -150,19 +192,36 @@ void QepcadCls::MONOTONE(Word A, Word J, Word D, Word r)
         // find polynomials:
         // Gs = g_2,...,g_{k-1} define proj_{k-1}(C).
         Gs = ZeroPols(AA, r, C, Ij + 1, Ik - 1);
-        Word GS1 = Gs, P, i = Ij;
-        while (GS1 != NIL) {
-            ADV(GS1, &P, &GS1);
-            LWRITE(P); SWRITE("\n");
+        Word LL = Gs, P, Q, i = Ij; // TODO
+        while (LL != NIL) {
+            ADV(LL, &P, &LL);
             IPDWRITE(i, P, GVVL); SWRITE("\n");
+            Q = Substitute(i, P, S0);
+            IPDWRITE(i+1-Ij, Q, GVVL); SWRITE("\n");
+
             i++;
         }
 
         // Fk (f_{k,T}, f_{k,B}) are 0 on CT and CB respectively.
+        FT  = ZeroPols(AA, r, CT, Ik, Ik);
+        FB  = ZeroPols(AA, r, CB, Ik, Ik);
+        SWRITE("f_top := "); IPDWRITE(Ik, FIRST(FT), GVVL); SWRITE("\n");
+        Q = Substitute(Ik, FIRST(FT), S0);
+        SWRITE("  subst := "); IPDWRITE(Ik + 1 - Ij, Q, GVVL); SWRITE("\n");
+        SWRITE("f_bottom := "); IPDWRITE(Ik, FIRST(FB), GVVL); SWRITE("\n");
+        Q = Substitute(Ik, FIRST(FB), S0);
+        SWRITE("  subst := "); IPDWRITE(Ik + 1 - Ij, Q, GVVL); SWRITE("\n");
 
-        // Fs = (f_{K+1},...,f_n) define C.
+        // Fs = (f_{K+1},...,f_n) is a map from proj_{k}(C) to R^{n-k}, of which C is the graph.
+        Fs = ZeroPols(AA, r, C, Ik + 1, r);
+        LL = Fs, i = Ik + 1;
+        while (LL != NIL) {
+            ADV(LL, &P, &LL);
+            IPDWRITE(i, P, GVVL); SWRITE("\n");
+            i++;
+        }
 
-        // semi-monotone
+        // semi-monotone -- Lagrange on f_T and f_B
 
         // monotone
     }

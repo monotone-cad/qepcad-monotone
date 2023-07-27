8e247298c42903ac7d489f96072812a0253fbcb2
Hollie Baker <hollieabaker@gmail.com>
2023-07-27 16:48:04 +0100

    quasiaffine, partials -> smooth strat

--- source/proj/QUASIAFFINE.c
+++ source/proj/QUASIAFFINE.c
@@ -17,25 +17,65 @@ SideEffect
 ======================================================================*/
 #include "qepcad.h"
 
-Word PARTIALS(Word k, Word P) {
-    Word L = IPALLPARTIALS(k, LELTI(P, PO_POLY), 1, 1);
+// return the index immediately after I in {0,1,...,M}^k with respect to lex order.
+// if no greater index exists, returns NIL
+// helper for LexNext, to ensure it stops at maximum index
+Word LexNext(Word I, Word M)
+{
+    Word m, I1, J1;
+    m = FIRST(I);
+    I1 = RED(I);
 
-    Word LL = NIL, D;
-    while (L != NIL) {
-        ADV(L, &D, &L);
-
-        // skip constant derivatives
-        if (IPCONST(k, D)) continue;
-
-	   LL = COMP(MPOLY(D,NIL,LIST1(LIST3(PO_DER,0,P)),PO_POLY,PO_KEEP), LL);
+    // increment m, if we can
+    if (m < M) {
+        return COMP(m + 1, I1);
     }
 
-    return LL;
+    // maximal index
+    if (I1 == NIL) {
+        return NIL;
+    }
+
+    // try to roll over
+    J1 = LexNext(I1, M);
+
+    if (J1 == NIL) return NIL; // fail, maximal index
+
+    return COMP(0, J1);
+}
+
+// return list 0^k
+Word ZEROS(Word k)
+{
+    Word L = NIL;
+    while (k > 0) {
+        L = COMP(0, L);
+        --k;
+    }
+
+    return L;
+}
+
+// list of all partials, sufficient for smooth stratification
+Word PARTIALS(Word r, Word d, Word L)
+{
+    Word LL, k;
+    k = LENGTH(L);
+
+    // indices
+    Word I = ZEROS(r);
+    while (I != NIL) {
+        LWRITE(I); SWRITE("\n");
+
+        I = LexNext(I, d);
+    }   SWRITE("\n");
+
+    return NIL;
 }
 
 void QepcadCls::QUASIAFFINE(Word A, Word r, Word *A_)
 {
-    Word AA, A1, A11, k, L;
+    Word AA, A1, A11, P, k, d, r1, L;
 
 Step1: /* decide based on dimension */
     if (r <= 1) {
@@ -46,22 +86,24 @@ Step1: /* decide based on dimension */
 
 Step3: /* dim >= 2: all partials of input polynomials */
     /* levels. */
-    AA = A; k = 0;
+    AA = A, k = NIL, k = 0, r1 = r, d = 0;
     while (AA != NIL) {
         ADV(AA, &A1, &AA);
-        L = NIL;
-        k++;
+        ++k; --r1;
 
         /* polynomials. */
         while (A1 != NIL) {
             ADV(A1, &A11, &A1);
+            P = LELTI(A11, PO_POLY);
 
-            L = CONC(L, PARTIALS(k, A11));
+            d = MAX(d, PDEG(P));
+            L = COMP(PADDVS(LELTI(A11, PO_POLY), r1), L);
         } /* END polynomials. */
-
-        // factorise and append -- same function as used on input formula
-	    ADDPOLS(IPLFAC(k, L),k,LFS("D"), &A);
     } /* END level. */
+
+    PARTIALS(r, d, L);
+    // factorise and append -- same function as used on input formula
+    //ADDPOLS(IPLFAC(k, L),k,LFS("D"), &A);
 
 Return: /* prepare for return */
     // put proj fac in correct order

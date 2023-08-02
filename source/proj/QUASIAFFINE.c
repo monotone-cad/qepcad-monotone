f8a609082128227e91234ea68ce8970bb610760d
Hollie Baker <hollieabaker@gmail.com>
2023-08-02 10:11:59 +0100

    add all partial differentials of input polynomials

--- source/proj/QUASIAFFINE.c
+++ source/proj/QUASIAFFINE.c
@@ -102,6 +102,56 @@ inline Word LSUM(Word L)
     return sum;
 }
 
+// construct a jacobi (k + 1) * (k + 1)-matrix from a minor (k*k-matrix) by appending one column
+// (\H_k / \x_j,...,\H_1) and one row (\P / \x_j, \P / \x_ik,..., \P / \x_i1)
+Word JacobiFromMinor(Word r, Word P, Word j, Word Hs, Word Is, Word Minor)
+{
+    // Step 1: add column
+    Word Jacobi = NIL; // list of k rows
+    Word k = 0; // dimension of Minor
+
+    Word H, Row;
+    while (Minor != NIL) {
+        ADV(Minor, &Row, &Minor);
+        ADV(Hs, &H, &Hs);
+        ++k;
+
+        // compute \H / \x_j
+        printf("IPDER(%d, H, %d)\n", r, j);
+        Word H1 = IPDER(r, H, j);
+
+        // append H1 to row
+        Row = COMP(H1, Row);
+        Jacobi = COMP(Row, Jacobi);
+    }
+
+    // Jacobi was constructed backwards
+    Jacobi = INV(Jacobi);
+
+    Word i;
+    Row = NIL;
+    while (k > 0) { // Is is assumed to have k+1 elements
+        ADV(Is, &i, &Is);
+        --k;
+
+        // compute \P / \x_i and append
+        printf("IPDER(%d, P, %d)\n", r, i);
+        Word P1 = IPDER(r, P, i);
+        Row = COMP(P1, Row);
+    }
+
+    // row constructed in order (i1,...,ik)
+    Row = INV(Row);
+
+    // compute \P / \x_j and append
+    printf("IPDER(%d, H, %d)\n", r, j);
+    Word P1 = IPDER(r, P, j);
+    Row = COMP(P1, Row);
+
+    // add row to jacobi matrix and return
+    return COMP(Row, Jacobi);
+}
+
 // recursive smooth stratification of polynomials Fs
 // np: number of polynomials
 // r: number of variables
@@ -117,6 +167,7 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
     // end of recursion
     Word i0 = FIRST(Is); // number of variables considered so far
     if (np == 0 || i0 == r) {
+        printf("...return. no recursion\n");
         return NIL;
     }
 
@@ -174,7 +225,7 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
 
         // compute s_k = partial_{(h_1,...,h_{k-1}),(i_1,...,i_{k-1}),v} h_k
         Word v = Dvs[p_index]; // differentiation variable
-        printf("v = %d\n", v);
+        printf("v = %d, round = %d\n", v, LENGTH(Is));
         Word ch_index = Chase_index[p_index]; // TODO debugging
 
         // maxed out index, no more differentials possible -- skip
@@ -190,9 +241,13 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
         Word D = Degrees[p_index];
         Word P = FIRST(*Ps[p_index]);
 
-        // TODO partial differential operator with h1,...,hk
-        // throw out constant polynomials
-        Word Q = IPDER(r, P, v);
+        // construct jacobi matrix using h1 = P and i1 = v
+        SWRITE("before jacobi "); LWRITE(Is); SWRITE("\n");
+        Word Jacobi = JacobiFromMinor(r, P, v, Hs, Is, Minor);
+        SWRITE("after jacobi "); LWRITE(Is); SWRITE("\n");
+
+        // compute partial differential, determinant of jacobi matrix
+        Word Q = MAIPDE(r, Jacobi); // next derivative is the jacobi determinant
         Word Qdeg = DEG(r,Q);
 
         if (LSUM(Qdeg) == 0) { // s_k is constant, may as well be zero
@@ -207,9 +262,11 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
         LWRITE(INDEX(count, D)); SWRITE(" "); Q == 0 ? SWRITE("0") : LWRITE(Q); SWRITE("\n\n");
 
         if (Q != 0) {
-            printf("recurse...\n");
-            Word Gs2 = STRAT(g_count, r, Gs, COMP(v, Is), COMP(P, Hs), Minor);
+            // Gs2 contains derivatives computed during recursion
+            SWRITE("before recursion "); LWRITE(Is); SWRITE("\n");
+            Word Gs2 = STRAT(g_count, r, Gs, COMP(v, Is), COMP(P, Hs), Jacobi);
             Gs1 = CONC(Gs1, Gs2);
+            SWRITE("after recursion "); LWRITE(Is); SWRITE("\n");
 
             // append new derivative to working list Gs and return list
             printf("add derivative to Gs %d\n", g_count);
@@ -246,7 +303,9 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
     Word i = g_count;
     while (i > 0) { // comp backwards
         --i;
-        Gs1 = COMP(FIRST(Gs[i][0]), Gs1);
+        // future functions expect QEPCAD polynomials
+        Word P1 = MPOLY(FIRST(Gs[i][0]), NIL, NIL, PO_POLY, PO_KEEP);
+        Gs1 = COMP(P1, Gs1);
     }
 
     // free Gs
@@ -255,6 +314,7 @@ Word STRAT(Word np, Word r, Word** Fs, Word Is, Word Hs, Word Minor)
     }
 
     free(Gs);
+    printf("...return\n");
 
     // returns list of derivatives
     return Gs1;
@@ -282,13 +342,13 @@ Word PARTIALS(Word r, Word L)
     }
 
     // initial i0 = FIRST(I1) = 0. h0 = FIRST(Hs) = 0, Minor is the empty matrix
-    STRAT(k, r, Fs, LIST1(0), LIST1(0), NIL);
+    Word Gs = STRAT(k, r, Fs, LIST1(0), LIST1(0), NIL);
 
     // free 2d Fs
     for (j = 0; j < k; j++) free(Fs[j]);
     free(Fs);
 
-    return NIL;
+    return Gs;
 }
 
 void QepcadCls::QUASIAFFINE(Word A, Word r, Word *A_)
@@ -298,31 +358,28 @@ void QepcadCls::QUASIAFFINE(Word A, Word r, Word *A_)
 Step1: /* decide based on dimension */
     if (r <= 1) {
         // nothing to do
-
-        goto Return;
+        *A_ = A;
+        return;
     }
 
-Step3: /* dim >= 2: all partials of input polynomials */
-    /* levels. */
+    // collect every polynomial in the input set, every polynomial will be in r variables regardless of lever
     AA = A, L = NIL, k = 0, r1 = r;
     while (AA != NIL) {
         ADV(AA, &A1, &AA);
         ++k; --r1;
 
-        /* polynomials. */
         while (A1 != NIL) {
             ADV(A1, &A11, &A1);
             P = LELTI(A11, PO_POLY);
 
             L = COMP(PADDVS(LELTI(A11, PO_POLY), r1), L);
-        } /* END polynomials. */
-    } /* END level. */
+        }
+    }
 
-    PARTIALS(r, L);
+    Word LL = PARTIALS(r, L);
     // factorise and append -- same function as used on input formula
-    //ADDPOLS(IPLFAC(k, L),k,LFS("D"), &A);
+    ADDPOLS(IPLFAC(k, LL),k,LFS("D"), &A);
 
-Return: /* prepare for return */
     // put proj fac in correct order
     *A_ = A;
 }

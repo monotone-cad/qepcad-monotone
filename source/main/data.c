146a97ef4866ee11ac1ce78b4927508386740614
Hollie Baker <hollieabaker@gmail.com>
2023-12-27 13:06:37 +0000

    remove PCFRT, frontier depends on monotone

--- source/main/data.c
+++ source/main/data.c
@@ -65,7 +65,6 @@ Word PCDBLIMIT;
 Word PCRESALG;    /* Choice of resultant algorithm. */
 Word PCIPFZT;     /* 'y' if IPFZT is turned on, 'n' otherise. */
 Word PCMCT;       /* 'y' if we want monotone cells, 'n' otherwise. */
-Word PCFRT;       /* 'y' if we want the frontier condition, 'n' otherwise. */
 Word PCVERBOSE;   /* Verbose mode: 0 means not verbose, 1 means verbose 06/19/02 */
 Word PCCONTINUE;   /* Flag to show that the "continue" command has been issued. */
 
@@ -162,7 +161,6 @@ void QepcadCls::INITGLOBALS()
   PCUSEDES  = 'n';
   PCIPFZT = 'y';
   PCMCT = 'n';
-  PCFRT = 'n';
   PCNUMDEC = 10;
   PCSIMPLIFY = 'b';
   PCEQC  = 0;

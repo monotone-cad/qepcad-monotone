2cc1d465a89a9eed7feb050c39b2d6a6a5da708c
Hollie Baker <hollieabaker@gmail.com>
2024-04-30 10:30:15 +0100

    clean up, ready to redo quasi-affine

--- source/proj/QUASIAFFINE.c
+++ source/proj/QUASIAFFINE.c
@@ -1,8 +1,8 @@
 /*======================================================================
   QUASIAFFINE(A, r, *A_);
 
-Adds first partial derivatives of projections onto coordinate axes, to result in 2d cells which are either increasing,
-decreasing or constant with respect to coordinate axes.
+considers the critical points of projections of the smooth two-dimensional locus of input set, onto one and two
+dimensional coordinate subspaces.
 
 \Input
   \parm{A} set of projection factors (A_1,...,A_r), each A_i is a list of polynomials in Z[x_1,...,x_i]
@@ -18,55 +18,14 @@ SideEffect
 ======================================================================*/
 #include "qepcad.h"
 
-// all nonconstant partial derivatives of polynomial P, in Z[x_1,...,x_k]
-Word PARTIALS(Word k, Word P)
-{
-    Word L = IPALLPARTIALS(k, LELTI(P, PO_POLY), 1, 1);
-
-    Word LL = NIL, D;
-    while (L != NIL) {
-        ADV(L, &D, &L);
-
-        // skip constant derivatives
-        if (IPCONST(k, D)) continue;
-
-	   LL = COMP(MPOLY(D,NIL,LIST1(LIST3(PO_DER,0,P)),PO_POLY,PO_KEEP), LL);
-    }
-
-    return LL;
-}
-
 void QepcadCls::QUASIAFFINE(Word A, Word r, Word *A_)
 {
-    Word AA, A1, A11, k, L;
+    // strat methos:
+    // - construct all sign sets of polynomials. 3^n of them. some will be empty
+    // - for each sign set, call stratify.
+    // - get back a list of strata.
+    // - for each 2-dimensional stratum, add jacobi det for each of the sets of variables, excluding two.
 
-    // nothing to do
-    if (r <= 1) {
-        *A_ = A;
-
-        return;
-    }
-
-    /* consider projection factors at each level */
-    AA = A; k = 0;
-    while (AA != NIL) {
-        ADV(AA, &A1, &AA);
-        ++k;
-
-        // consider each polynomial
-        L = NIL;
-        while (A1 != NIL) {
-            ADV(A1, &A11, &A1);
-
-            // adding each partial derivative
-            L = CONC(L, PARTIALS(k, A11));
-        }
-
-        // and factorise (same function as used with the input formula)
-	    ADDPOLS(IPLFAC(k, L),k,LFS("D"), &A);
-    }
-
-    /* prepare for return */
     *A_ = A;
 }
 

3b92c3c9fd5e6fb125778d7e2084ec0e68b87ca0
holliebaker <30434642+holliebaker@users.noreply.github.com>
2025-02-28 10:28:33 +0000

    badness check before trying lazard

--- source/main/QEPCAD.c
+++ source/main/QEPCAD.c
@@ -146,6 +146,11 @@ Step5: /* Monotone cells, if needed */
         // refine the CAD
         D = REFINE(1, D, GVREFL, P);
 
+        /* Perform badness check, in which case extension to Lazard isn't guaranteeed to work */
+        if (BADCHECK(D)) {
+            FAIL("QEPCAD", "A bad cell of dimension > 0 exists.");
+        }
+
         // if we cached an ESPCAD, it will no longer be valid. If CAD was projection definable before, it will be still
         // be projection definable now.
         if (GVTD != NIL && FIRST(GVTD) == 0) {
@@ -155,11 +160,6 @@ Step5: /* Monotone cells, if needed */
         FRONTIER(f, 1, D, P, &P, &J, &RPs);
         GVREFL = RPs;
         D = REFINE(1, D, GVREFL, P);
-
-        /* Perform badness check */
-        if (BADCHECK(D)) {
-            FAIL("QEPCAD", "A bad cell of dimension > 0 exists.");
-        }
 
         /*Int*/ if (PCCONTINUE == TRUE) { goto Return; }
     }
